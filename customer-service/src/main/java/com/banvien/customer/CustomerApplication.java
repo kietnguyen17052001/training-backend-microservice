1d5c134dca195e06cf0af4a80d4277babc168a7e
Nguyen Dang Kiet <kiet.nguyen-dang@internal.banvien.com>
2022-07-25 09:47:22 +0700

    jdbc backend

--- customer-service/src/main/java/com/banvien/customer/CustomerApplication.java
+++ customer-service/src/main/java/com/banvien/customer/CustomerApplication.java
@@ -2,10 +2,18 @@ package com.banvien.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+import javax.sql.DataSource;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@ComponentScan("com.banvien.customer.repository")
 @RestController
 public class CustomerApplication {
     public static void main(String[] args) {
@@ -16,4 +24,5 @@ public class CustomerApplication {
     public String display() {
         return new String("customer-service");
     }
+
 }
--- product-service/src/main/java/com/banvien/product/ProductApplication.java
+++ product-service/src/main/java/com/banvien/product/ProductApplication.java
@@ -2,10 +2,13 @@ package com.banvien.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@ComponentScan("com.banvien.product.repo")
 @RestController
 public class ProductApplication {
     public static void main(String[] args) {
